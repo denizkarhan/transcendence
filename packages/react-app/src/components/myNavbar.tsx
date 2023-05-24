fe1a16a78321c4b983c58c38f33ad177665a0065
Taha Haksal <m.haksal@gmail.com>
2023-05-24 10:31:45 +0300

    asd

--- packages/react-app/src/components/myNavbar.tsx
+++ packages/react-app/src/components/myNavbar.tsx
@@ -1,31 +1,39 @@
-import React from 'react';
-import axios from 'axios';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { Link } from 'react-router-dom';
-import './Nav.css'
+import React from "react";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Dropdown from "react-bootstrap/Dropdown";
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
-export default async function MyNavbar() {
+export default function MyNavbar() {
   const linkStyle = {
     margin: "1rem",
     textDecoration: "none",
-    color: 'blue'
+    color: "blue",
   };
 
-  const user = await axios.get('/user');
+  const user = axios.get("/user");
 
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Link to="/home" className="navbar-brand">Winx Club</Link>
+        <Link to="/home" className="navbar-brand">
+          Winx Club
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="">
-            <Link style={linkStyle} type=""to="home">Home</Link>
-            <Link style={linkStyle} to="login">Sign in</Link>
-            <Link style={linkStyle} to="register">Sign up</Link>
+            <Link style={linkStyle} type="" to="home">
+              Home
+            </Link>
+            <Link style={linkStyle} to="login">
+              Sign in
+            </Link>
+            <Link style={linkStyle} to="register">
+              Sign up
+            </Link>
           </Nav>
           <Nav className="">
             <Dropdown>
@@ -34,13 +42,19 @@ export default async function MyNavbar() {
               </Dropdown.Toggle>
               <Dropdown.Menu>
                 <Dropdown.Item>
-                  <Link style={linkStyle} to="profile">Profile</Link>
+                  <Link style={linkStyle} to="profile">
+                    Profile
+                  </Link>
                 </Dropdown.Item>
                 <Dropdown.Item>
-                  <Link style={linkStyle} to="logout">Log out</Link>
+                  <Link style={linkStyle} to="logout">
+                    Log out
+                  </Link>
                 </Dropdown.Item>
                 <Dropdown.Item>
-                  <Link style={linkStyle} to="settings">Settings</Link>
+                  <Link style={linkStyle} to="settings">
+                    Settings
+                  </Link>
                 </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
